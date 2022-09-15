c4556e8e81efbc9d7cfea5225bdb7ec85a5e0b22
evgnor86 <atxn@yandex.ru>
2022-09-15 15:19:00 +0300

    First commit

---
+++ gse-ver-upd.py
@@ -0,0 +1,21 @@
+import os
+import json
+
+
+def run(base_dir: str, version: str) -> None:
+
+    dirs = [dir_name for dir_name in os.listdir(base_dir) if dir_name[0] != '.']
+    # print(json.dumps(dirs, indent=4))
+
+    for dir_name in dirs:
+        metadata = json.load(open(f'{base_dir}/{dir_name}/metadata.json'))
+        metadata['shell-version'].append(version) if version not in metadata['shell-version'] else None
+        print(metadata['shell-version'])
+        json.dump(metadata, open(f'{base_dir}/{dir_name}/metadata.json', 'w'), indent=4)
+
+
+    return
+
+
+if __name__ == '__main__':
+    run('./extensions/', '43')
