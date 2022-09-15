f702f78f861be68def737e4e06255aed6b6713bc
evgnor86 <atxn@yandex.ru>
2022-09-15 15:26:42 +0300

    First commit

--- gse-ver-upd.py
+++ gse-ver-upd.py
@@ -4,17 +4,14 @@ import json
 
 def run(base_dir: str, version: str) -> None:
 
-    dirs = [dir_name for dir_name in os.listdir(base_dir) if dir_name[0] != '.']
+    dirs: list = [dir_name for dir_name in os.listdir(base_dir) if dir_name[0] != '.']
     # print(json.dumps(dirs, indent=4))
 
     for dir_name in dirs:
         metadata = json.load(open(f'{base_dir}/{dir_name}/metadata.json'))
         metadata['shell-version'].append(version) if version not in metadata['shell-version'] else None
-        print(metadata['shell-version'])
+        print(f'{dir_name} {metadata["shell-version"]}')
         json.dump(metadata, open(f'{base_dir}/{dir_name}/metadata.json', 'w'), indent=4)
-
-
-    return
 
 
 if __name__ == '__main__':
