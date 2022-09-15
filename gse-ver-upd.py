4c509e21d10324d6be8a5175d9708303588d51d0
evgnor86 <atxn@yandex.ru>
2022-09-15 18:23:13 +0300

    First commit

--- gse-ver-upd.py
+++ gse-ver-upd.py
@@ -2,14 +2,16 @@ import os
 import json
 
 
-def run(base_dir: str, version: str) -> None:
+def run(base_dir: str, shell_version: str) -> None:
 
     dirs: list = [dir_name for dir_name in os.listdir(base_dir) if dir_name[0] != '.']
 
+    print(f'\n{"GNOME Shell Extension name:":50} {"Extension version:":25} GNOME Shell versions:\n')
     for dir_name in dirs:
         metadata = json.load(open(f'{base_dir}/{dir_name}/metadata.json'))
-        metadata['shell-version'].append(version) if version not in metadata['shell-version'] else None
-        print(f'{dir_name} {metadata["shell-version"]}')
+        metadata['shell-version'].append(shell_version) if shell_version not in metadata['shell-version'] else None
+        version = str(metadata['version']) if 'version' in metadata.keys() else ''
+        print(f'{metadata["name"]:50} {version:25} {metadata["shell-version"]}')
         json.dump(metadata, open(f'{base_dir}/{dir_name}/metadata.json', 'w'), indent=4)
 
 
